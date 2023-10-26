60fa8c8400667e5df55adc8e6bdf99ce7508eb92
gatituhackery <petergatitu61@gmail.com>
2023-10-26 13:26:17 -0700

    new

---
+++ 100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+    unsigned int num = 1;
+    char *endian = (char *)&num;
+
+    if (*endian)
+        return 1; /* Little endian */
+    else
+        return 0; /* Big endian */
+}
